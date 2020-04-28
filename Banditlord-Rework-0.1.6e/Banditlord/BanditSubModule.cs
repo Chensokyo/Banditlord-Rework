4b40c9f90d5efb6092e795ead137a1ae526a7ea9
Chensokyo <63869075+Chensokyo@users.noreply.github.com>
2020-04-28 16:10:37 +0200

    Add files via upload

---
+++ Banditlord-Rework-0.1.6e/Banditlord/BanditSubModule.cs
@@ -0,0 +1,25 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace BanditSubModule
+{
+    class BanditSubModule : MBSubModuleBase
+    {
+
+        public static readonly string ModuleName = "BanditTroops";
+
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+        }
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            if (!(game.GameType is Campaign))
+                return;
+            CampaignGameStarter gameInitializer = (CampaignGameStarter)gameStarterObject;
+
+            gameInitializer.AddBehavior(new BanditTroops.BanditTroops());
+        }
+    }
+}
